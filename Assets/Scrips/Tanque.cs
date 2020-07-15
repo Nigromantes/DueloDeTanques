6694a3fedd111852353d8d200dd0587c1332adeb
Nigromantes <nicolasbru@hotmail.com>
2020-07-15 14:20:27 -0500

    Primer Commit - Se ha iniciado la estructura de la clase tanque y sus subclases

---
+++ Assets/Scrips/Tanque.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tanque
+{
+    //Esto sería una propiedad.
+    private string nombre;
+
+    public string Nombre
+    {
+        get
+        {
+            return nombre;
+        }
+        set
+        {
+            nombre = value;
+        }
+
+    }
+
+
+    public class Cañón
+    {
+        private int probabilidadDeFallo;
+
+        public int ProbabilidadDeFallo
+        {
+            get
+            {
+                return probabilidadDeFallo;
+            }
+
+            set
+            {
+                probabilidadDeFallo = value;
+            }
+
+
+        }
+
+        private int tiempoDeDisparo;
+
+        public int TiempoDeDisparo
+        {
+            get
+            {
+                return TiempoDeDisparo;
+            }
+
+            set
+            {
+
+                tiempoDeDisparo = value;
+            }
+
+
+
+        }
+
+        private int tiempoDeRecarga;
+
+        public int TiempoDeRecarga
+        {
+
+            get
+            {
+                return tiempoDeRecarga;
+
+            }
+
+            set
+            {
+
+                tiempoDeRecarga = value;
+            }
+
+        }
+        private int precisionDeDisparo;
+
+        public int PrecisionDeDisparo
+        {
+
+            get
+            {
+
+                return precisionDeDisparo;
+            }
+            set
+            {
+                precisionDeDisparo = value; 
+            
+            }
+
+
+        }
+
+    }
+
+    public class Bala
+    { 
+    
+    
+    }
+
+    public class Estructura
+    { 
+    
+    
+    }
+
+    public class Tripulación
+    { 
+    
+    
+    }
+    
+
+}
+
+
