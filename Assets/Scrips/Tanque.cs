27bafbe2485ed2488b8e87651aa4e750e4afa9bc
Nigromantes <49623001+Nigromantes@users.noreply.github.com>
2020-07-20 15:44:33 -0500

    Se ha adelantado una parte de las subclases de la clase Tanque

--- Assets/Scrips/Tanque.cs
+++ Assets/Scrips/Tanque.cs
@@ -99,21 +99,145 @@ public class Tanque
     }
 
     public class Bala
-    { 
-    
-    
+    {
+        private int energiaDeImpacto;
+        public int EnergiaDeImpacto 
+        {
+            get
+            {
+                return energiaDeImpacto;
+            }
+
+
+            set
+            {
+                energiaDeImpacto = value;
+            }
+                
+                
+        }
+
+        public int correcionDeTractoria;
+
+        public int CorreccionDeTractoria
+        {
+
+            get 
+            {
+                return correcionDeTractoria;
+            }
+
+            set 
+            {
+                correcionDeTractoria = value;            
+            
+            }
+        
+        }
+
+
     }
 
     public class Estructura
-    { 
-    
+    {
+        private int reduccionDeGolpe;
+
+        public int ReduccionDeGolpe
+        {
+            get
+            {
+                return reduccionDeGolpe;
+            }
+            set
+            {
+                reduccionDeGolpe = value;
+            }
+
+
+
+        }
+
+
+        private int tiempoDeVibracion;
+
+        public int TiempoDeVibracion
+        {
+            get
+            {
+                return tiempoDeVibracion;
+            }
+            set 
+            {
+                tiempoDeVibracion = value;
+            }
+        
+        
+        }
     
     }
 
     public class Tripulación
-    { 
-    
-    
+    {
+        //Eficiencia
+        private int eficiencia;
+        public int Eficiencia
+        {
+            get 
+            {
+                return eficiencia;
+            }
+            set
+            {
+                eficiencia = value;
+            }
+        }
+        //Liderzgo. 
+
+        private int liderzago;
+        public int Liderzago
+        {
+            get
+            {
+                return liderzago;
+            }
+            set
+            {
+                liderzago = value;
+            }
+        }
+        
+
+        //Entreanmiento físico. 
+        public class Entrenamiento
+        {
+            //Energia
+            private int energia;
+            public int Energia
+            {
+                get
+                {
+                    return energia;
+                }
+                set
+                {
+                    energia = value;
+                }
+            }
+
+            //Recuperación del cansacio. 
+            //Equilibrio
+            //Recupración de caída. 
+            //Probaabilidad de mareo o confusión. 
+            //Recuperación de mareo o confusión. 
+
+
+        }
+
+
+
+
+
+
     }
     
 
